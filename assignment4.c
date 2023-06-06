286a336aa6f251e50bd115f1843e1d2bd5047276
yangcan1 <yangca@oregonstate.edu>
2023-06-05 19:26:35 -0700

    changes

--- assignment4.c
+++ assignment4.c
@@ -8,18 +8,18 @@ pthread_cond_t myCond1 = PTHREAD_COND_INITIALIZER;
 pthread_cond_t myCond2 = PTHREAD_COND_INITIALIZER;
 int myCount = 0;
 
+// Producer function only add 1 into myCount, and wait for consumer.
 void* producer() {
 
     while (1) {
-        
 
-        pthread_mutex_lock(&myMutex); // Lock the mutex before checking if the buffer has data
+        pthread_mutex_lock(&myMutex); // Lock the mutex before adding 1.
         printf("PRODUCER: myMutex locked\n");
 
         myCount++;
 
         printf("PRODUCER: waiting on myCond1\n");
-        pthread_cond_wait(&myCond1, &myMutex);
+        pthread_cond_wait(&myCond1, &myMutex); // wait for condition variable 1 which will be signalled by consumer. 
     
         if (myCount == 10) {
             break;
@@ -29,20 +29,20 @@ void* producer() {
 
         
         printf("PRODUCER: signaling myCond2\n");
-        pthread_cond_signal(&myCond2); // Signal consumer that the buffer is no longer empty
-        // sleep(1);
+        pthread_cond_signal(&myCond2); // Signal consumer that myCount has been added.
     }
 
     return NULL;
 }
 
-void* consumer() {
+// Consumer function only prints out myCount -> myCount. After that, wait for Producer.
+void* consumer() { 
     printf("CONSUMER THREAD CREATED\n");
 
     pthread_mutex_lock(&myMutex); // Lock the mutex before checking if the buffer has data
     printf("CONSUMER: myMutex locked\n");
 
-    while (1) { // Buffer is empty. Wait for signal that the buffer has data
+    while (1) {
         
         printf("myCoung: %d -> %d\n", myCount - 1, myCount);
 
@@ -50,15 +50,14 @@ void* consumer() {
         printf("CONSUMER: myMutex unlocked\n");
 
         printf("CONSUMER: signaling myCond1\n");
-        pthread_cond_signal(&myCond1);
+        pthread_cond_signal(&myCond1); // signaling cond1 to producer that count has been printed out.
 
         if (myCount == 10) {
             break;
         }
         printf("CONSUMER: waiting on myCond2\n");
-        pthread_cond_wait(&myCond2, &myMutex);
+        pthread_cond_wait(&myCond2, &myMutex); // wait for cond2 that will be signaled by producer.
 
-        
     }
     
     return NULL;
@@ -76,7 +75,6 @@ int main() {
     pthread_join(pTh, NULL);
     pthread_join(cTh, NULL);
 
-    
     printf("PROGRAM END\n"); // Program ends
     return 0;
 }
